49c17ac5058df094bfb0964cb1bf5f4ce148d157
sungwoo091228 <75023231+sungwoo091228@users.noreply.github.com>
2021-05-08 09:29:29 +0900

    Add files via upload

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇실행이 시작되었습니다(24시간 온라인).")
-    game = discord.Game('~하는 중 ex)봇만들기 하는 중')
+    game = discord.Game('픽온 제작중..')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -25,10 +25,10 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == 디스코드ID를 적기!!:
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="최상단 제목")
-                        embed.add_field(name="제목", value=msg, inline=True)
-                        embed.set_footer(text=f"서버초대코드")
+                    if message.author.id == 705771803989245993:
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="디엠봇공지")
+                        embed.add_field(name="테스트중", value=msg, inline=True)
+                        embed.set_footer(text=f"https://discord.gg/zHGFgZwszA")
                         await i.send(embed=embed)
                 except:
                     pass
